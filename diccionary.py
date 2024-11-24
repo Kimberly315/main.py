b177c4d738ed53e8d684ca406d07d2eb5153a36c
Kimberly315 <cantislove123@gmail.com>
2024-11-24 10:34:35 -0500

    Create diccionary.py

---
+++ diccionary.py
@@ -0,0 +1,15 @@
+diccionario={
+    "JUICIOSO":"persona responsable",
+    "CHIMBA":"algo que las espectativas o algo bonito",
+    "CRISPETAS":"también conocidas como , rosas, pochoclos, cabritas",
+    "AGUACATE":"también conocido como palta"
+}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+#print(diccionario.keys())
+if word in diccionario.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(diccionario.keys[word])
+ else:
+    print('la palabra que busca está en un territorio desconocido')
